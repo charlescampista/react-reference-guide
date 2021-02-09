d3fe1a54a14b9fda3cf9f178052f308cb52d3c6d
Charles <charlescampista@gmail.com>
2021-02-09 04:58:48 -0300

    Working with CSS modules

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import styled from 'styled-components'; //RETURNS A REACT COMPONENT WHENEVER USED
+//import styled from 'styled-components'; //RETURNS A REACT COMPONENT WHENEVER USED
 
+import classes from './Person.css'
 //import './Person.css';
 
 const person = (props)  =>  {
@@ -12,29 +13,29 @@ const person = (props)  =>  {
     // }
 
     //styled.div DOWN BELLOW RETURNS A REACT COMPONENT.
-    const StyledDiv = styled.div`
+    // const StyledDiv = styled.div`
 
-    width: 60%;
-    margin: 16px auto;
-    border: 1px solid #eee;
-    box-shadow: 0 2px 3px #ccc;
-    padding: 16px;
-    text-align: center;
+    // width: 60%;
+    // margin: 16px auto;
+    // border: 1px solid #eee;
+    // box-shadow: 0 2px 3px #ccc;
+    // padding: 16px;
+    // text-align: center;
 
-    @media (min-width: 500px) {
-        width: 450px;
-    }
-    `
+    // @media (min-width: 500px) {
+    //     width: 450px;
+    // }
+    // `
 
     return (
         //<div className="Person" style={style}>
         
-        <StyledDiv>
+        <div className={classes.Person}>
             <p onClick={props.click}>I'm {props.name} and I am  {props.age} years old!</p>
             <p>{props.click}</p>
             <input type="text" onChange={props.changed} value={props.name}/>
             <p>{ props.name }</p> 
-        </StyledDiv>
+        </div>
 
     );
     
